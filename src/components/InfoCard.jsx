9644fe3281aec0ada7056d8bb3d144e927274404
DeepanshuRohilla15 <20001001027deepanshu@dcrustm.org>
2023-03-28 00:02:18 +0530

    Improving in infocard

--- src/components/InfoCard.jsx
+++ src/components/InfoCard.jsx
@@ -6,10 +6,10 @@ const InfoCard = () => {
   return (
     <div style={{ textAlign: 'center', padding: '0 10%'}}>
         Try saying: <br /> 
-        Add {isIncome ? 'Income' : 'Expense'}
-        for {isIncome ? '$100' : '$50'} 
-        in Category {isIncome ? 'Business' : 'House'} 
-        for {isIncome ? 'Monday' : 'Tuesday'}
+        Add {isIncome ? 'Income ' : 'Expense '}
+        for {isIncome ? '$100 ' : '$50 '} 
+        in Category {isIncome ? 'Business ' : 'House '} 
+        for {isIncome ? 'Monday ' : 'Tuesday '}
     </div>
   )
 }
