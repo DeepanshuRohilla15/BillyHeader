a5ae8f6ae453d9fffcf7231032049f92bd47b90d
DeepanshuRohilla15 <20001001027deepanshu@dcrustm.org>
2023-03-28 00:50:20 +0530

    Improvements

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -12,7 +12,7 @@ const Main = () => {
       const { balance } = useContext(ExpenseTrackerContext);
   return (
     <Card className={classes.root}>
-            <CardHeader title="Expense Tracker" subheader="Powered by Speechly" />
+            <CardHeader title="Expense Tracker" subheader="Powered by React" />
             <CardContent>
                   <Typography align="center" variant="h5" >Total Balance ${balance}</Typography>
                   <Typography variant="subtitle1" style={{ lineHeight: '1.5em', marginTop: '20px' }}>
