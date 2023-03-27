5980f8193852f2378b2f0195b347285f9850f7a5
DeepanshuRohilla15 <20001001027deepanshu@dcrustm.org>
2023-03-27 22:44:57 +0530

    Greeting user with prevoius transactions

--- src/context/context.js
+++ src/context/context.js
@@ -1,7 +1,7 @@
 import React, { useReducer, createContext  } from 'react';
 import contextReducer from './contextReducer';
 
-const initialState = [{ amount: 5, category: 'Savings',type: 'Income',date: '27-03-2023', id: '1'}];
+const initialState = JSON.parse(localStorage.getItem('transactions')) ||  [[{"amount":125,"category":"Clothes","type":"Expense","date":"2023-03-27","id":"48dd9340-0060-40d2-973d-8c7126741010"},{"amount":200,"category":"Gifts","type":"Income","date":"2023-03-27","id":"997e4d00-0798-4752-9fd3-7837de7c91af"},{"amount":50,"category":"Lottery","type":"Income","date":"2023-03-27","id":"b58dfaf6-8b3a-459d-b6e1-9c4e76ff5656"},{"amount":1000,"category":"Salary","type":"Income","date":"2023-03-28","id":"f31dd374-3045-4db0-900b-db79ebce5bff"},{"amount":1000,"category":"Pets","type":"Expense","date":"2023-04-03","id":"9f18ae50-c260-49a8-b78e-e0dde57bdef5"}]];
 
 export const ExpenseTrackerContext = createContext(initialState);
 
