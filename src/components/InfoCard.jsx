ae5d3fa7dbcd0b8e89e6e7559ff50a39cc75f27c
DeepanshuRohilla15 <20001001027deepanshu@dcrustm.org>
2023-03-27 23:59:23 +0530

    Dynamic infocard created

--- src/components/InfoCard.jsx
+++ src/components/InfoCard.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
+const isIncome = Math.round(Math.random());
+
 const InfoCard = () => {
   return (
     <div style={{ textAlign: 'center', padding: '0 10%'}}>
         Try saying: <br /> 
-        ADD income
-        for $100 
-        in Category Salary 
-        for Monday ...
+        Add {isIncome ? 'Income' : 'Expense'}
+        for {isIncome ? '$100' : '$50'} 
+        in Category {isIncome ? 'Business' : 'House'} 
+        for {isIncome ? 'Monday' : 'Tuesday'}
     </div>
   )
 }
