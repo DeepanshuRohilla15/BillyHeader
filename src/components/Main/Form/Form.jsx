ea2c0cb4c06dc3953d6fe0b3dcf414df182c0e89
DeepanshuRohilla15 <20001001027deepanshu@dcrustm.org>
2023-03-27 23:44:02 +0530

    Adding created notifications

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -7,6 +7,7 @@ import { useSpeechContext } from '@speechly/react-client';
 import formatDate from '../../../utils/formatDate';
 import useStyles from './styles';
 import { incomeCategories, expenseCategories } from '../../../constants/categories';
+import CustomizedSnackbar from '../../Snackbar/Snackbar';
 
 const initialState = {
       amount: '',
@@ -21,10 +22,13 @@ const Form = () => {
       const [formData, setFormData] = useState(initialState);
       const { addTransaction } = useContext(ExpenseTrackerContext);
       const { segment } = useSpeechContext();
+      const [open, setOpen] = useState(false);
 
       const createTransaction = () => {
             if(Number.isNaN(Number(formData.amount)) || !formData.date.includes('-')) return;
             const transaction = { ...formData, amount: Number(formData.amount), id: uuidv4()}
+
+            setOpen(true);
             addTransaction(transaction);
             setFormData(initialState);
       }
@@ -74,6 +78,7 @@ const Form = () => {
 
   return (
     <Grid container spacing={2}>
+      <CustomizedSnackbar open={open} setOpen={setOpen} />
       <Grid item xs={12}>
             <Typography align="center" variant="subtitle2" gutterBottom>
             {segment ? (
